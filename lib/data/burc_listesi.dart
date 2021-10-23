e78f1890eb7a4ba8f632d7392d9c1970270c280e
AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>
2021-10-23 18:50:19 +0300

    23 Ekim 2021 Flutter Burc Rehberi
    Signed-off-by: AbdurrahmanKaraoglu <18110131307@ogr.ksu.edu.tr>

---
+++ lib/data/burc_listesi.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BurcListesi extends StatelessWidget {
+  const BurcListesi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Burç Listesi Buraya Gelecek"),
+    );
+  }
+}
